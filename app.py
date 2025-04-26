0df0d1ed4fe20a845f7608ceda5fe09d51e83505
UnoWataru <uno.wataru.0633@gmail.com>
2025-04-27 00:58:00 +0900

    closing, intradayの選択を追加

--- app.py
+++ app.py
@@ -15,7 +15,12 @@ tickers = {
     'PHLX Semiconductor':'^SOX',
     'NYSE FANG+TM Index': '^NYFANG',
 }
-index= st.selectbox("Index", tickers.keys())
+col_ticker, col_category = st.columns((1, 1))
+with col_ticker:
+    index= st.selectbox("Index", tickers.keys())
+with col_category:
+    category = st.selectbox("Category", ['Intraday', 'Closing'])
+    intraday = (category == 'Intraday')
 ticker = tickers[index]
 
 data = yf.download(ticker, start='2000-01-01', auto_adjust=False).xs(ticker, axis=1, level=1)
@@ -24,7 +29,7 @@ unit = '$'
 
 price = data['Adj Close'].to_frame('price').dropna()
 price['max'] = price['price'].expanding().max()
-price['ath'] = price['price'] == price['max']
+price['ath'] = (price['price'] == price['max'])
 price = price.iloc[1:]
 start = price[price['ath']].index[0]
 price = price.loc[start:].copy()
@@ -34,45 +39,61 @@ st.write('# Status')
 with st.container(border=True):
     st.write('## Current Price')
     nowtime, now_price = price.index[-1], price['price'].iloc[-1]
-    now_day = yf.download(
-        ticker, start=f'{nowtime:%Y-%m-%d}', end=f'{nowtime+pd.Timedelta(days=1):%Y-%m-%d}',
-        interval='1m', auto_adjust=False).xs(ticker, axis=1, level=1)
-    now_day = now_day.tz_convert('America/New_York')
-    nowtime, now_price = now_day.index[-1], now_day.iloc[-1]['Close']
+    nowtime = nowtime + pd.Timedelta(hours=16)
+    if intraday:
+        now_day = yf.download(
+            ticker, start=f'{nowtime:%Y-%m-%d}', end=f'{nowtime+pd.Timedelta(days=1):%Y-%m-%d}',
+            interval='1m', auto_adjust=False).xs(ticker, axis=1, level=1)
+        now_day = now_day.tz_convert('America/New_York')
+        nowtime, now_price = now_day.index[-1] + pd.Timedelta(minutes=1), now_day.iloc[-1]['Close']
     st.write(f'{nowtime:%Y-%m-%d %H:%M} : {unit}{now_price:,.2f}')
 
     st.write('## All-Time-High')
-    ath = data['High'].sort_values().iloc[[-1]]
+    name = 'Close'
+    if intraday:
+        name = 'High'
+    ath = data[name].sort_values().iloc[[-1]]
     ath_time, ath_price = ath.index[-1], ath.iloc[-1]
-    try:
-        ath_day = yf.download(
-            ticker, start=f'{ath_time:%Y-%m-%d}', end=f'{ath_time+pd.Timedelta(days=1):%Y-%m-%d}',
-            interval='1m', auto_adjust=False).xs(ticker, axis=1, level=1)
-        ath_day = ath_day.tz_convert('America/New_York')
-        ath_time = ath_day['High'].sort_values().index[-1]
-        st.write(f'{ath_time:%Y-%m-%d} : {unit}{ath_price:,.2f}')
-    except:
-        st.write(f'{ath_time:%Y-%m-%d} : {unit}{ath_price:,.2f}')
+    ath_time = ath_time + pd.Timedelta(hours=16)
+    message = f'{ath_time:%Y-%m-%d %H:%M} : {unit}{ath_price:,.2f}'
+    if intraday:
+        try:
+            ath_day = yf.download(
+                ticker, start=f'{ath_time:%Y-%m-%d}', end=f'{ath_time+pd.Timedelta(days=1):%Y-%m-%d}',
+                interval='1m', auto_adjust=False).xs(ticker, axis=1, level=1)
+            ath_day = ath_day.tz_convert('America/New_York')
+            ath_time = ath_day['High'].sort_values().index[-1]
+            message = f'{ath_time:%Y-%m-%d %H:%M} : {unit}{ath_price:,.2f}'
+        except:
+            message = f'{ath_time:%Y-%m-%d} : {unit}{ath_price:,.2f}'
+    st.write(message)
 
     st.write('## Recent Low')
+    name = 'Close'
+    if intraday:
+        name = 'Low'
     start = price[price['term'] == price['term'].max()].index[0]
     end = price[price['term'] == price['term'].max()].index[-1]
-    recent_low = data.loc[start:end, 'Low'].sort_values().iloc[[0]]
+    recent_low = data.loc[start:end, name].sort_values().iloc[[0]]
     recent_low_time, recent_low_price = recent_low.index[0], recent_low.iloc[0]
-    try:
-        recent_low_day = yf.download(
-            ticker, start=f'{recent_low_time:%Y-%m-%d}', end=f'{recent_low_time+pd.Timedelta(days=1):%Y-%m-%d}',
-            interval='1m', auto_adjust=False).xs(ticker, axis=1, level=1)
-        recent_low_day = recent_low_day.tz_convert('America/New_York')
-        recent_low_time = recent_low_day['Low'].sort_values().index[0]
-        st.write(f'{recent_low_time:%Y-%m-%d %H:%M} : {unit}{recent_low_price:,.2f}')
-    except:
-        st.write(f'{recent_low_time:%Y-%m-%d} : {unit}{recent_low_price:,.2f}')
+    recent_low_time = recent_low_time + pd.Timedelta(hours=16)
+    message = f'{recent_low_time:%Y-%m-%d %H:%M} : {unit}{recent_low_price:,.2f}'
+    if intraday:
+        try:
+            recent_low_day = yf.download(
+                ticker, start=f'{recent_low_time:%Y-%m-%d}', end=f'{recent_low_time+pd.Timedelta(days=1):%Y-%m-%d}',
+                interval='1m', auto_adjust=False).xs(ticker, axis=1, level=1)
+            recent_low_day = recent_low_day.tz_convert('America/New_York')
+            recent_low_time = recent_low_day['Low'].sort_values().index[0]
+            message = f'{recent_low_time:%Y-%m-%d %H:%M} : {unit}{recent_low_price:,.2f}'
+        except:
+            message = f'{recent_low_time:%Y-%m-%d} : {unit}{recent_low_price:,.2f}'
+    st.write(message)
 
 st.write(f"- All-time-high is `{(ath_price - now_price) /now_price:.2%}` above the current price.")
 st.write(f"- Current price is at `{(now_price -recent_low_price) / (ath_price - recent_low_price):.2%}` between recent low and all-time-high.")
 
-st.write('# Price Movement since ATH(close)')
+st.write('# Price Movement since ATH(Closing)')
 cols = st.columns((1, 1))
 with cols[0]:
     from_year = st.selectbox(
@@ -103,7 +124,8 @@ for start, end in zip(ath_dates, ath_dates[1:] + [None]):
         hovertemplate="%{customdata}"
     )]
 fig = go.Figure(data=dat)
-fig.update_traces(selector=-1,line=dict(color='black',width=3))
+fig.update_traces(line={'width': 1})
+fig.update_traces(selector=-1, line={'color':'black', 'width':2})
 fig.update_layout(xaxis={'title': 'days from ATH'},
                  yaxis={'title': '% from ATH'})
 st.plotly_chart(fig)
